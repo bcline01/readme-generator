2d65c87b5e476dd855cad43d1fb1d675c318f20b
bcline01 <brookecline.bc@gmail.com>
2024-07-26 12:10:17 -0400

    completed .js and working on markdown file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,8 +15,44 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description
+  ${data.desc}
 
+  ## Table of Contents
+  1. [Description](#description)
+  2. [Installation](#installation)
+  3. [Usage](#usage)
+  4. [Credits](#credits)
+  5. [Test](#test)
+  6. [Contact](#contact)
+  7. [License](#license)
+
+  ## Installation
+  ${data.install}
+  
+
+  ## Usage
+  ${data.use}
+
+
+  ## Credits
+  ${data.credits}
+  
+
+  ## Test
+  ${data.test}
+
+
+  ## Contact
+  Email: ${data.email}   
+  Github: https://github.com/${data.github} 
+
+
+  ## License
+  ${data.license} 
 `;
 }
+//export module
+
 
 export default generateMarkdown;
--- utils/index.js
+++ utils/index.js
@@ -1,48 +1,92 @@
 // TODO: Include packages needed for this application
 import fs from 'fs';
 import inquirer from 'inquirer'; 
+import generateMarkdown from './generateMarkdown.js';
+
 
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
 
-inquirer.prompt([
     {
         type: 'input',
-        message: "Provide a short description of your project",
+        message: "What is the Title of your project?",
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: "Provide a short description of your project.",
         name: 'description',
     },
     {
-        type: 'checkbox',
-        message: "what would you like to include in the table of contents?",
-        name: 'contents',
-        choices: ['installation','lisence','credits']
-    },
-    {
         type: 'input',
-        message: "what are the steps to install your project",
+        message: "What are the steps to install your project?",
         name: 'install',
     },
     {
         type: 'input',
-        message: "List any credits for this project",
+        message: "Please provide instructions and examples for use.",
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: "List any credits for this project.",
         name: 'credits',
     },
     {
         type: 'list',
-        message: "what lisence would you like to use for your project?",
+        message: "What lisence would you like to use for your project?",
         name: 'lisence',
         choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'NONE']
-    }
-])
-.then((answers) => console.log(answers));
+    },
+    {
+        type: 'input',
+        message: "Badges that you would like to include.",
+        name: 'badges',
+    },
+    {
+        type: 'input',
+        message: "List any features used in your project.",
+        name: 'features',
+    },
+    {
+        type: 'input',
+        message: "Please list any contributors. (Github usernames)",
+        name: 'contribute',
+    },
+    {
+        type: 'input',
+        message: "How to test the project.",
+        name: 'test',
+    },
+    {
+        type: 'input',
+        message: "Frequently Asked Questions.",
+        name: 'questions',
+    },
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, 'utf8', (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Success!');
+        }
+    });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// initailizing app
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        console.log("Creating Professional README.md File");
+        const markdown = generateMarkdown(answers); 
+        writeToFile('../dist/README.md', markdown); 
+    });
+}
 
 // Function call to initialize app
 init();
+
